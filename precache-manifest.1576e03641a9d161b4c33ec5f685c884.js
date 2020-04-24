e558a9c9ea7060fdd281b16aa9866e5624c4bbf6
tehpunisher456 <55720542+tehpunisher456@users.noreply.github.com>
2020-04-24 00:30:42 -0700

    Updates

---
+++ precache-manifest.1576e03641a9d161b4c33ec5f685c884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0360692c05cbfedab1db0d82962578",
+    "url": "/week-19/index.html"
+  },
+  {
+    "revision": "4692401b6b8300df6e27",
+    "url": "/week-19/static/js/2.30209078.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/week-19/static/js/2.30209078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba6710e90dd7e33cabe8",
+    "url": "/week-19/static/js/main.0166f8bb.chunk.js"
+  },
+  {
+    "revision": "b9b18c619c3f3570aec8",
+    "url": "/week-19/static/js/runtime-main.52362ad6.js"
+  }
+]);
